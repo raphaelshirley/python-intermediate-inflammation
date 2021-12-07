be513e4fb4ce7cbc001a7f7f9b042c75aceeb889
Raphael Shirley <raphael.shirley@hotmail.co.uk>
2021-12-07 11:39:56 +0000

    Updated unit tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -99,4 +99,84 @@ def test_daily_min(test, expected):
     npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
 
+# @pytest.mark.parametrize(
+#     "test, expected",
+#     [
+#         ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
+#     ])
+
+# @pytest.mark.parametrize(
+#     "test, expected",
+#     [
+#         ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]),
+#         ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]]),
+#         ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]]),
+#     ])
+
+# @pytest.mark.parametrize(
+#     "test, expected",
+#     [
+#         (
+#             [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+#             [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+#         ),
+#         (
+#             [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+#             [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+#         ),
+#         (
+#             [[float('nan'), 1, 1], [1, 1, 1], [1, 1, 1]],
+#             [[0, 1, 1], [1, 1, 1], [1, 1, 1]],
+#         ),
+#         (
+#             [[1, 2, 3], [4, 5, float('nan')], [7, 8, 9]],
+#             [[0.33, 0.67, 1], [0.8, 1, 0], [0.78, 0.89, 1]],
+#         ),
+#         (
+#             [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+#             [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+#         ),
+#         (
+#             [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+#             [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+#         )
+#     ])
+# def test_patient_normalise(test, expected):
+#     """Test normalisation works for arrays of one and positive integers.
+#        Assumption that test accuracy of two decimal places is sufficient."""
+#     from inflammation.models import patient_normalise
+#     npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+
+@pytest.mark.parametrize(
+    "test, expected, raises",
+    [
+        (
+            'hello',
+            None,
+            TypeError,
+        ),
+        (
+            3,
+            None,
+            TypeError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        )
+    ])
+def test_patient_normalise(test, expected, raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+    if isinstance(test, list):
+        test = np.array(test)
+    if raises:
+        with pytest.raises(raises):
+            npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(test), np.array(expected), decimal=2)
+
+
+
 # TODO(lesson-robust) Implement tests for the other statistical functions
